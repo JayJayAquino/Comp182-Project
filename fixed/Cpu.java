54ed23ac11d63a41eb5ebb79c8be527c4552ea7f
skydrop14 <runeletter14@gmail.com>
2018-08-09 12:30:03 -0700

    Cpu, Checkout, and User

--- fixed/Cpu.java
+++ fixed/Cpu.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Cpu{
    public static void log(String s){
       System.out.println(s); 
@@ -12,64 +14,87 @@ class Cpu{
    }
 
    public void cpuList(){
-      log("please choose what CPU are you going to buy");
-      log("[0] Quit [1] [2] [3] [4] [5] [6] [7] [8] [9]");
+	  Scanner myScanner = new Scanner(System.in);
+	  log("please choose what CPU are you going to buy");
+      // put array list(show the list of cpu
       int cList = 0;
       boolean cExit = false;
+      int cPrice = 0;
+      int deliver = 0;
+      
+      
       //boolean 
       while(!cExit){
-            switch(cList){
-            
+    	  boolean selection;
+    	  do{//do
+    		  try{//try   
+    			  cList = Integer.parseInt(myScanner.nextLine());
+                  selection = false;
+              }//try
+              catch(Exception e){//catch
+            	  log("please type in number");
+                  selection = true;
+              }//catch
+    	  }while(selection);//do +while
+    	  switch(cList){  
             case 0:
             	log("you decided to quit whith out chosing your cpu");
             	cExit = true;
-            	
+            	cPrice += 0;
             	break;
             	
             case 1 :
-                log("you chose to buy");                
-            
-
+                log("you chose to buy ----and the price will be ----");                
+                cPrice += 0;
+                cExit = true;
             break;
             
             case 2 :
                 log("you chose to buy");                
-            
+                cPrice += 0;
+                cExit = true;
             break;	
 
             case 3 :
                 log("you chose to buy");                
-            
+                cPrice += 0;
+                cExit = true;
             break;	
 
             case 4 :
                 log("you chose to buy");                
-            
+                cPrice += 0;
+                cExit = true;
             break;	
 
             case 5 :
                 log("you chose to buy");                
-            
+                cPrice += 0;
+                cExit = true;
             break;	
 
             case 6 :
                 log("you chose to buy");                
-            
+                cPrice += 0;
+                cExit = true;
             break;	
 
             case 7 :
                 log("you chose to buy");                
-            
+                cPrice += 0;
+                cExit = true;
             break;	
 
             case 8 :
                 log("you chose to buy");                
-            
+                cPrice += 0;
+                cExit = true;
             break;
 
             case 9 :
                 log("you chose to buy");                
-            
+                cPrice += 0;
+                cExit = true;
             break;	
 
 
