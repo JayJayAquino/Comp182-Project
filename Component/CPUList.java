f792eb5d1c8bf8497f9fb1f6e830fb32a172ac82
JayJayAquino <36612928+JayJayAquino@users.noreply.github.com>
2018-08-12 15:06:22 -0700

    Testing CPU Lists

--- Component/CPUList.java
+++ Component/CPUList.java
@@ -1,13 +1,15 @@
 //CPU sub-class
-
+import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Iterator;
 
 public class CPUList{
   static public void main(String[] args){
     ArrayList<String> cpuList = new ArrayList<String>();
+    ArrayList<String> testList = new ArrayList<String>();
     Iterator<String> iter;
 
+    cpuList.add("Return to Select screen");
     cpuList.add("CPU1");
     cpuList.add("CPU2");
     cpuList.add("CPU3");
@@ -19,7 +21,9 @@ public class CPUList{
     cpuList.add("CPU9");
     cpuList.add("CPU10");
 
-    System.out.println("Number of CPU's available: " + cpuList.size());
+    int size = cpuList.size() - 1;
+
+    System.out.println("Number of CPU's available: " + size);
 
     System.out.println("List of CPU's: ");
     iter = cpuList.listIterator();
@@ -28,5 +32,19 @@ public class CPUList{
       System.out.println(cpuList.indexOf(nextItem)+") "
       + nextItem);
     }
+
+    Scanner reader = new Scanner(System.in);
+    System.out.print("Enter a number: ");
+    int n = reader.nextInt();
+    for(int i = 0; i <= size-1; i++ )
+    {
+      if(n == 0){
+        //return
+      }else if(n == i){
+        testList.add(cpuList.get(n));
+        System.out.println(testList.get(0));
+      }
+    }
+    reader.close();
   }
 }
